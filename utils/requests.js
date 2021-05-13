72a067680854f1978fbd8c2bde4a06a2a6a30663
Abigail T. Cameron <75511255+AbigailTCameron@users.noreply.github.com>
2021-05-12 23:19:43 -0500

    Update requests.js

--- utils/requests.js
+++ utils/requests.js
@@ -39,21 +39,21 @@ export default {
 
     fetchSciFi:{
         title: 'Sci-Fi',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=878`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=878`
     },
 
     fetchWestern:{
         title: 'Western',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=37`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=37`
     },
 
     fetchAnimation:{
         title: 'Animation',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=16`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=16`
     },
 
     fetchTV:{
         title: 'TV Shows',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=10770`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`
     },
 };
